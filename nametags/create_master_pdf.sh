cf1554d7bad1345c01d1d82e4677ecd50cf3d992
Christian Schulz <christian.schulz.phone@gmail.com>
2022-06-24 13:54:56 +0200

    .

--- nametags/create_master_pdf.sh
+++ nametags/create_master_pdf.sh
@@ -7,12 +7,14 @@ HEADEND=28
 TAILSTART=0
 TAILEND=0
 
+OVERALLSTRING=""
 while read p; do 
         FIRSTNAME=`echo $p | awk 'BEGIN {FS=","} {print $1}'`;
         LASTNAME=`echo $p | awk 'BEGIN {FS=","} {print $2}'`;
         AFFILIATION=`echo $p | awk 'BEGIN {FS=","} {print $3}'`;
 
 
+        echo $LASTNAME $COUNTER
         sed -n '1,28 p' a7-60.tex > badge$COUNTER.tex
         echo "\\Large\\textbf{$FIRSTNAME $LASTNAME} \\\\
 \\large\\textit{$AFFILIATION}" >> badge$COUNTER.tex
@@ -21,7 +23,8 @@ while read p; do
         pdflatex badge$COUNTER.tex
         #> badge$COUNT.tex
 
+        OVERALLSTRING=`echo -ne $OVERALLSTRING badge$COUNTER.pdf`
         COUNTER=`echo $COUNTER +1 | bc`
 done < data.csv
 
-pdftk badge*.pdf cat output masterbadge.pdf
+pdftk $OVERALLSTRING cat output masterbadge.pdf
